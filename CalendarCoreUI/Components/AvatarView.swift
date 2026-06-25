6297a9464baa021056f8327d035771cdbe33cd77
Baptiste Griva <baptiste.griva@infomaniak.com>
2026-06-25 14:58:04 +0200

    feat: Add AccountCellView

---
+++ CalendarCoreUI/Components/AvatarView.swift
@@ -0,0 +1,54 @@
+/*
+ Infomaniak Calendar - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import InfomaniakCore
+import InfomaniakCoreSwiftUI
+import NukeUI
+import SwiftUI
+
+public struct AvatarView: View {
+    let user: UserProfile
+    let size: CGFloat
+
+    public init(user: UserProfile, size: CGFloat) {
+        self.user = user
+        self.size = size
+    }
+
+    public var body: some View {
+        if let rawAvatarURL = user.avatar,
+           let avatarURL = URL(string: rawAvatarURL) {
+            LazyImage(request: ImageRequest(url: avatarURL)) { state in
+                if let image = state.image {
+                    AvatarImage(image: image, size: size)
+                } else {
+                    initialsView
+                }
+            }
+        }
+    }
+
+    private var initialsView: some View {
+        InitialsView(
+            initials: NameFormatter(fullName: user.displayName).initials,
+            backgroundColor: Color.backgroundColor(from: user.email.hash),
+            foregroundColor: Color.white,
+            size: size
+        )
+    }
+}
---
+++ CalendarFeatures/CalendarListView/AccountCellView.swift
@@ -0,0 +1,45 @@
+/*
+ Infomaniak Calendar - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import CalendarCore
+import CalendarCoreUI
+import InfomaniakCore
+import InfomaniakCoreSwiftUI
+import SwiftUI
+
+struct AccountCellView: View {
+    let account: CalendarAccount
+    let avatarSize: CGFloat = 40
+
+    var body: some View {
+        HStack {
+            AvatarView(user: account.user, size: avatarSize)
+
+            VStack(alignment: .leading, spacing: 0) {
+                Text(account.user.displayName)
+                    .font(.body)
+                    .fontWeight(.medium)
+                Text(account.user.email)
+                    .font(.body)
+                    .foregroundStyle(.gray)
+            }
+            .lineLimit(1)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
