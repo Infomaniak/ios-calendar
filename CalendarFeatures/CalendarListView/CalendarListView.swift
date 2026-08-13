4d0174c1ff81fcba3340aacff2ed4c90bddc0693
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-08-13 08:37:49 +0200

    feat: Add account list

--- CalendarFeatures/CalendarListView/CalendarListView.swift
+++ CalendarFeatures/CalendarListView/CalendarListView.swift
@@ -44,7 +44,7 @@ public struct CalendarListView: View {
             }
 
             Section {
-                NavigationLink(destination: AccountSettingsView()) {
+                NavigationLink(destination: AccountsListContentView()) {
                     Label {
                         Text(CalendarResourcesStrings.accountsTitle)
                             .foregroundStyle(theme.color.contentPrimary)
