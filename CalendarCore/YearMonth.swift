69f60a7c78e0e317c71432b62d9217c90048f3f4
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2026-08-20 08:40:48 +0200

    chore: Update KMP

---
+++ CalendarCore/YearMonth.swift
@@ -0,0 +1,34 @@
+/*
+ Infomaniak Calendar - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+
+public struct YearMonth: Sendable, Hashable {
+    public let year: Int
+    public let month: Int
+
+    public init(year: Int, month: Int) {
+        self.year = year
+        self.month = month
+    }
+
+    public static func fromDate(date: Date, calendar: Calendar) -> YearMonth? {
+        let components = calendar.dateComponents([.year, .month], from: date)
+        return YearMonth(year: components.year!, month: components.month!)
+    }
+}
