e8fed3bc374d2b348a2f9441b1efa7206128c723
Baptiste Griva <baptiste.griva@infomaniak.com>
2026-08-25 16:07:37 +0200

    chore: Clean CalendarRow component

--- CalendarFeatures/EventDetailsView/EventCalendarRow.swift
+++ CalendarFeatures/EventDetailsView/EventCalendarRow.swift
@@ -19,28 +19,27 @@
 import CalendarCoreUI
 import CalendarResources
 import DesignSystem
+import ESDSFoundation
 import InfomaniakCoreSwiftUI
 import InfomaniakDI
 @preconcurrency import MultiplatformCalendar
 import SwiftUI
 
-struct CalendarSectionView: View {
+struct EventCalendarRow: View {
+    @Environment(\.esdsTheme) private var theme
+
     @State private var selectedCalendar: UICalendar?
-    @State private var availableCalendars: [UICalendar] = []
-    @Binding private var calendarColor: Color
 
     let event: CalendarCoreUI.UIEvent?
 
     init(
         event: CalendarCoreUI.UIEvent?,
-        calendarColor: Binding<Color> = .constant(.gray)
     ) {
         self.event = event
-        _calendarColor = calendarColor
     }
 
     var body: some View {
-        Section {
+        VStack {
             if let selectedCalendar {
                 LabeledContent {
                     HStack {
@@ -53,11 +52,16 @@ struct CalendarSectionView: View {
                             .lineLimit(1)
                     }
                 } label: {
-                    Text(CalendarResourcesStrings.sectionCalendarHeader)
+                    HStack {
+                        CalendarResourcesAsset.Images.productCalendar.swiftUIImage
+                            .iconSize(IKIconSize.large)
+                            .foregroundStyle(theme.color.contentSecondary)
+                        Text(CalendarResourcesStrings.sectionCalendarHeader)
+                            .font(.body)
+                            .foregroundStyle(theme.color.contentPrimary)
+                    }
                 }
             }
-        } header: {
-            Text(CalendarResourcesStrings.sectionCalendarHeader)
         }
         .task {
             await observeCalendars()
@@ -67,17 +71,15 @@ struct CalendarSectionView: View {
     private func observeCalendars() async {
         @InjectService var calendarSDK: CalendarCoreGraph
         for await calendars in calendarSDK.calendarManager.observeCalendars() {
-            let uiCalendars = calendars.map { UICalendar(calendar: $0) }
-            availableCalendars = uiCalendars
+            let availableCalendars = calendars.map { UICalendar(calendar: $0) }
 
             if let event {
-                selectedCalendar = uiCalendars.first { $0.id == event.calendarId }
+                selectedCalendar = availableCalendars.first { $0.id == event.calendarId }
             }
-            calendarColor = selectedCalendar?.color ?? .gray
         }
     }
 }
 
 #Preview {
-    CalendarSectionView(event: UIEvent.preview)
+    EventCalendarRow(event: UIEvent.preview)
 }
