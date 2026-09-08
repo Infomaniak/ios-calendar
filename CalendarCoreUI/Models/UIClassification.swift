44b5c8e5569efa9d84dc4297a00b72df4b631ee6
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-09-08 11:20:28 +0200

    feat: Add Label extension to use CalendarResourcesImages

--- CalendarCoreUI/Models/UIClassification.swift
+++ CalendarCoreUI/Models/UIClassification.swift
@@ -19,7 +19,6 @@
 import CalendarResources
 import Foundation
 import MultiplatformCalendar
-import SwiftUI
 
 public enum UIClassification: String, Sendable, CaseIterable {
     case `public`
@@ -38,12 +37,12 @@ public enum UIClassification: String, Sendable, CaseIterable {
         }
     }
 
-    public var icon: Image? {
+    public var icon: CalendarResourcesImages? {
         switch self {
         case .public:
-            return CalendarResourcesAsset.Images.lockOpen.swiftUIImage
+            return CalendarResourcesAsset.Images.lockOpen
         case .private:
-            return CalendarResourcesAsset.Images.lock.swiftUIImage
+            return CalendarResourcesAsset.Images.lock
         default:
             return nil
         }
